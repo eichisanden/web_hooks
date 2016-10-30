96c49ad33fcda7670b0feb8d2432f33922de0685
eichisanden <eichisanden@gmail.com>
2016-10-31 00:05:28 +0900

    add log

--- main.go
+++ main.go
@@ -7,7 +7,19 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	fmt.Fprintf(w, "ref:"+r.FormValue("ref"))
+	fmt.Fprintf(w, "commits:"+r.FormValue("commits"))
+	fmt.Fprintf(w, "head_commit:"+r.FormValue("head_commit"))
+	fmt.Fprintf(w, "repository:"+r.FormValue("repository"))
+	fmt.Fprintf(w, "pusher:"+r.FormValue("pusher"))
+	fmt.Fprintf(w, "sender:"+r.FormValue("sender"))
+
+	fmt.Printf("ref:" + r.FormValue("ref"))
+	fmt.Printf("commits:" + r.FormValue("commits"))
+	fmt.Printf("head_commit:" + r.FormValue("head_commit"))
+	fmt.Printf("repository:" + r.FormValue("repository"))
+	fmt.Printf("pusher:" + r.FormValue("pusher"))
+	fmt.Printf("sender:" + r.FormValue("sender"))
 }
 
 func main() {
